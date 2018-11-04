c67b7e70f9fe7e025bae18b55447fd22fcdbe017
김대석 <greatstone@satreci.com>
2018-11-05 01:12:37 +0900

    문제 1181 풀이

---
+++ 1181/1181.py
@@ -0,0 +1,9 @@
+# 문제: https://www.acmicpc.net/problem/1181
+# 풀이: 문자열 길이와 문자열로 정렬
+import sys
+n = int(sys.stdin.readline())
+words = [sys.stdin.readline().rstrip() for _ in range(n)]
+sorted_words = sorted(words, key=lambda x: (len(x), x))
+for i in range(len(sorted_words)):
+    if i == 0 or sorted_words[i] != sorted_words[i - 1]:
+        print(sorted_words[i])
